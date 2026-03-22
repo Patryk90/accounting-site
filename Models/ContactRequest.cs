3404e76e0cf98233b4a48b89be49583774628560
Patryk LASKOWICZ <patryk.laskowicz@at.zurich.com>
2026-03-22 13:12:54 +0100

    Front end styling adjustments.

---
+++ Models/ContactRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountingPortfolio.Models;
+
+public class ContactRequest
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Message { get; set; }
+}
